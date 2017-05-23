d545d4bac969914d033d04bb8aaf5420522e536a
jeremie-passerat <jeremie.passerat@etu.univ-amu.fr>
2017-05-23 12:12:42 +0200

    Version en cours de l'exercice 6

--- src/main/java/fr/univ_amu/iut/exercice6/MinesweeperBoard.java
+++ src/main/java/fr/univ_amu/iut/exercice6/MinesweeperBoard.java
@@ -3,10 +3,15 @@ package fr.univ_amu.iut.exercice6;
 import java.util.List;
 
 public class MinesweeperBoard {
+
+    private List<String> resultBoard;
+
     public MinesweeperBoard(List<String> inputBoard) {
+        this.resultBoard=inputBoard;
     }
 
     public List<String> getAnnotatedRepresentation() {
-        return null;
+
+       return resultBoard;
     }
 }
